6909d8687092d006139cd1260126fe1a1a2ca89e
JosephMarzouk <joseph1marzok@gmail.com>
2024-02-07 19:59:45 +0200

    z

---
+++ lib/customWidgets/customSearchBar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:med_eg/constants/colors.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left:12,top:12),
+          child: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(34.5)),
+            child: Image.asset(
+              'assets/images/PNG/UserPhoto.png',
+              height: 47,
+              width: 47,
+              //color: Colors.black,
+            ),
+          ),
+        ),
+        title:const Padding(
+          padding: const EdgeInsets.only(top: 12),
+          child: Text('Hello..User'),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(14),
+        child: Column(children: [
+         // SearchBar(),
+        ],),
+      ),
+    );
+  }
+}
