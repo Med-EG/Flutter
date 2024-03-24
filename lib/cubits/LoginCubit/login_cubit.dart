32fbf123fadc94f7ff53faf3a563132c901702b3
JosephMarzouk <joseph1marzok@gmail.com>
2024-03-24 02:27:42 +0200

    unfinished login logic

--- lib/cubits/LoginCubit/login_cubit.dart
+++ lib/cubits/LoginCubit/login_cubit.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:med_eg/cubits/LoginCubit/login_states.dart';
+import 'package:med_eg/helper/API.dart';
+import 'package:med_eg/models/paitentModel.dart';
 
-class LoginCubit extends Cubit<LoginState>{
-  LoginCubit(super.initialState);
-
+class LoginCubit extends Cubit<LoginState> {
   
+  LoginCubit() : super(InitialState());
+
+  PaitentInfo ?paitent;  
+  
+  login({required String email, required String password}) async {
+    try {
+      final response = await Api().post(
+          url: 'https://api-medeg.online/medEG/patient/login',
+          body: {'email': email, 'password': password});
+      paitent = PaitentInfo.fromJson(response);
+      if (paitent != null) {
+        emit(Success(paitent: paitent!));
+      } else if(email==null){
+        emit(MissingData(message:'Fill all the fields please'));
+      }
+    } catch (responce) {
+      emit(InvalidData(message: 'email or password is wrong'));
+    }
+  }
 }
--- lib/cubits/LoginCubit/login_states.dart
+++ lib/cubits/LoginCubit/login_states.dart
@@ -1,7 +1,25 @@
-class LoginState{}
+import 'package:med_eg/models/paitentModel.dart';
 
+class LoginState {}
 
-class NoDataEnterd extends LoginState{}
-class InvalidData extends LoginState{}
-class Loading extends LoginState{}
-class Success extends LoginState{}
+class InitialState extends LoginState {}
+
+class MissingData extends LoginState {
+  final String message;
+
+  MissingData({required this.message});
+}
+
+class InvalidData extends LoginState {
+  final String message;
+
+  InvalidData({required this.message});
+}
+
+class Loading extends LoginState {}
+
+class Success extends LoginState {
+  final PaitentInfo paitent;
+
+  Success({required this.paitent});
+}
